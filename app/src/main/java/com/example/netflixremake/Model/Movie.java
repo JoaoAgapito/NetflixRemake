4b94932777dc13ba7627c64cef1bac569ccea8a8
João Victor Agapito <55551801+JoaoAgapito@users.noreply.github.com>
2022-06-29 07:44:30 -0300

    1.0

---
+++ app/src/main/java/com/example/netflixremake/Model/Movie.java
@@ -0,0 +1,14 @@
+package com.example.netflixremake.Model;
+
+public class Movie {
+
+    private int coverUrl;
+
+    public int getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(int coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+}
