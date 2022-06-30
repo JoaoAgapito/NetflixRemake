66295bc36accd3806d50da322f345e7fdfb26401
João Victor Agapito <55551801+JoaoAgapito@users.noreply.github.com>
2022-06-30 20:15:48 -0300

    2.0

--- app/src/main/java/com/example/netflixremake/MovieActivity.java
+++ app/src/main/java/com/example/netflixremake/MovieActivity.java
@@ -2,15 +2,28 @@ package com.example.netflixremake;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MovieActivity extends AppCompatActivity {
+
+    private TextView txtTitle;
+    private TextView txtDesc;
+    private TextView txtCast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        txtTitle = findViewById(R.id.text_view_title);
+        txtDesc = findViewById(R.id.text_view_desc);
+        txtCast = findViewById(R.id.text_view_cast);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -18,5 +31,21 @@ public class MovieActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
         }
+
+        LayerDrawable drawable =
+                (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.shadows);
+
+        if (drawable != null) {
+            Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie);
+            drawable.setDrawableByLayerId(R.id.cover_drawable, movieCover);
+            ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(drawable);
+            getSupportActionBar().setTitle(null);
+
+        }
+
+        txtTitle.setText("Batman Begins");
+        txtDesc.setText("Marcado pelo assassinato de seus pais quando ainda era criança, o milionário Bruce Wayne (Christian Bale) decide viajar pelo mundo em busca de encontrar meios que lhe permitam combater a injustiça e provocar medo em seus adversários. Após retornar a Gotham City, sua cidade-natal, ele idealiza seu alter-ego: Batman, um justiceiro mascarado que usa força, inteligência e um arsenal tecnológico para combater o crime.");
+        txtCast.setText(getString(R.string.cast, "Christian Bale, Katie Holmes, Michael Caine"));
+
     }
 }
